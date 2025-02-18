36d2d987b0507938c1f0dc2bc3dbc6dd50da4c65
Mosa-1994 <197264025+Mosa-1994@users.noreply.github.com>
2025-02-18 22:24:17 +0100

    Added tailwind to file.

---
+++ src/categories.js
@@ -0,0 +1,15 @@
+// categories.js
+export const categories = {
+    'Basis Concepten': {
+      beschrijving: 'Fundamentele JavaScript concepten',
+      begrippen: ['variable', 'value']
+    },
+    'Functies & Parameters': {
+      beschrijving: 'Alles over functies en hun gebruik',
+      begrippen: ['function', 'parameter', 'method']
+    },
+    'Control Flow': {
+      beschrijving: 'Besturing van je code',
+      begrippen: ['loop', 'condition']
+    }
+  };
---
+++ src/data.js
@@ -0,0 +1,20 @@
+// data.js
+export const begrippen = {
+    'variable': {
+      engels: 'Variable',
+      nederlands: 'Variabele',
+      uitleg: 'Een container waarin je data kunt opslaan.',
+      voorbeeld: [
+        {
+          uitleg: 'Een variabele maken die je later kunt veranderen:',
+          code: 'let name = "John"'
+        },
+        {
+          uitleg: 'Een variabele maken die niet kan veranderen:',
+          code: 'const age = 25'
+        },
+        // ... rest van de voorbeelden
+      ]
+    },
+    // ... rest van de begrippen (zoals eerder gedefinieerd)
+  };
